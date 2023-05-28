7411adaeea1b10ae559a6e5946f00564b97a336e
thib_info <thibault.b45@gmail.com>
2023-05-28 17:08:42 +0200

    Add the connexion between the images picked and their link json file

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ Usage of Flask to handle multiple pages with better layout than in Dash
 
 from flask import Flask, render_template
 
-from src.card import get_random_images
+from src.card import get_random_images, get_names
 
 app = Flask(__name__, template_folder='templatesFiles', static_folder='staticFiles')
 
-
-card_bck = get_random_images()
+cards_bck = get_random_images()
+cards_name = get_names(cards_bck)
 
 @app.route('/')
 def home():
@@ -17,7 +17,8 @@ def home():
     data = {
         'title': 'Home Page',
         'message': 'Welcome to the home page!',
-        'cards_img': card_bck
+        'cards_img': cards_bck,
+        'cards_name': cards_name
     }
     return render_template('home.html', data=data)
 
