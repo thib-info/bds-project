6abfd66159829953f579aa4d4d1e2abcf55666af
thib_info <thibault.b45@gmail.com>
2023-05-31 01:03:40 +0200

    Revert "Merge pull request #15 from thib-info/hotfix-3cards"
    This reverts commit 08e9346ee4d17286041ef4b9e351db175a83d60e, reversing
    changes made to f5587177ed2240bb7894aabe6da0bdcb89e1992a.

--- app.py
+++ app.py
@@ -18,29 +18,25 @@ files_path = []
 cards_id = []
 cards_name = []
 cards_bck = []
-while ind < 3:
-    try:
-        file_path = get_random_files(1)
-        [card_id, card_name] = get_file_common_info(file_path)
-        card_bck = get_image_path(file_path)
+while correct is False and ind < 3:
+    file_path = get_random_files(1)
+    [card_id, card_name] = get_file_common_info(file_path)
+    card_bck = get_image_path(file_path)
 
-        if not os.path.exists(card_bck[0][1:]):
-            continue
+    if not os.path.exists(card_bck[0][1:]):
+        correct = False
+        continue
 
-        files_path.append(file_path[0])
-        cards_id.append(card_id[0])
-        cards_name.append(card_name[0])
-        cards_bck.append(card_bck[0])
+    files_path.append(file_path[0])
+    cards_id.append(card_id[0])
+    cards_name.append(card_name[0])
+    cards_bck.append(card_bck[0])
 
-        ind = ind + 1
-    except Exception as e:
-        print(e)
+    ind += 1
+    correct = True
 
 for file in files_path:
-    print("DEBUG")
-    print(file)
     cards_info[file] = extract_info(file)
-
 
 @app.route('/api/data')
 def get_data():
@@ -51,7 +47,7 @@ def get_data():
 def get_new_card():
     done = False
     global cards_info
-    while done is False:
+    while(done == False):
         try:
             new_card_path = get_random_files(1)
             [new_card_id, new_card_name] = get_file_common_info(new_card_path)
